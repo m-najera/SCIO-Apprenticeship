3579433adb85d20eb2132472bfef6e56c1cadb3b
Miguel Angel Najera Salinas <manajera@scio.local>
2019-03-04 09:39:39 -0600

    completed users page

--- AngularJS/controller/users.js
+++ AngularJS/controller/users.js
@@ -1,29 +1,60 @@
-app.controller("Users", function ($scope, $http) {
+app.controller("Users", function ($scope, $http, localStorageService) {
+  $scope.key = "superKey";
+  $scope.id = 0;
   $scope.list = [];
-  $scope.id = 13;
-  $scope.load = function () {
+  $scope.loadServer = () => {
     $http.get("https://reqres.in/api/users?page=4")
       .then(function (data) {
         $scope.list = data.data.data;
+        $scope.id = 13;
       }, function (error) {
         console("Error");
       });
   }
 
-  $scope.addUser = ()=> {
-    $scope.list.push($scope.newUser)
-    $scope.id++;
-    $scope.newUser= {
-      id: $scope.id,
-      first_name : "",
-      last_name :""
+  $scope.saveServer = () =>{
+    $http.post("https://reqres.in/api/users",$scope.list).then(data=>{
+      $scope.response = data.data;
+      console.log("INSERTADO")
+      console.log($scope.response);
+    },
+    error=>{
+      console.log("ERROR")
+    })
+  }
+
+  $scope.saveLocal = () => {
+    localStorageService.set($scope.key,$scope.list)
+  }
+
+  $scope.loadLocal = () => {
+    if (localStorageService.get($scope.key)) {
+      $scope.list = localStorageService.get($scope.key);
+      $scope.id = $scope.list[$scope.list.length - 1].id + 1;
+      
+    } else {
+      $scope.list = [];
     }
   }
 
-  $scope.newUser= {
-    id: $scope.id,
-    first_name : "",
-    last_name :""
+  $scope.addUser = () => {
+    $scope.newUser.id = $scope.id;
+    $scope.list.push($scope.newUser)
+    $scope.id++;
+    $scope.newUser = {
+      first_name: "",
+      last_name: ""
+    }
+  }
+
+  $scope.newUser = {
+    first_name: "",
+    last_name: ""
+  }
+
+  $scope.deleteUser = (i) => {
+    $scope.list.splice(i,1);
+    $scope.id = $scope.list[$scope.list.length - 1].id + 1;
   }
 
 })
